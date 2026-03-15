89de9fd0ef402bdec0860041e2531af208439fed
Abshoor24 <muhammadnurulabshoor24@gmail.com>
2026-03-15 23:28:25 +0700

    BAGIAN 5 PRODUCT

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 Route::apiResource('/categories', CategoryController::class);
 Route::apiResource('/products', ProductController::class);
+// BAGIAN 5 PRODUCT
